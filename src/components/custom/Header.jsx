5832c2e3e21a52e393b8d373844915b2c701c2c3
Pattaraphon1 <klgsa51@gmail.com>
2025-07-22 17:24:17 +0700

    created component of header and pages of about/contacts/coupons

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,6 +1,28 @@
+import { Link } from "react-router"
+
 function Header() {
   return (
-    <div>Header</div>
+    <>
+      <div className="flex items-center justify-center bg-[#003f66] text-white px-2 py-2 h-13 gap-15">
+
+        <div className="flex gap-7">
+          <Link to="/" className="hover:text-[#e59b2b] text-">Home</Link>
+          <Link to="/about" className="hover:text-[#e59b2b]">About Us</Link>
+          <Link to="/coupon" className="hover:text-[#e59b2b]">Coupons</Link>
+        </div>
+
+         <div className="flex gap-4">
+          <Link to="/" className="w-9 text-2xl" >Logo</Link>
+        </div>
+
+        <div className="flex items-center gap-7">
+          <Link to="/contact" className="hover:text-[#e59b2b]">Contact Us</Link>
+          <Link to="/login" className="hover:text-[#e59b2b]">Login</Link>
+          <Link to="/register" className="hover:text-[#e59b2b]" >Register</Link>
+        </div>
+
+      </div>
+    </>
   )
 }
 
