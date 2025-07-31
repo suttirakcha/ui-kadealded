69457877649701274b355348e3dfa520a4838b4e
Pattaraphon1 <klgsa51@gmail.com>
2025-07-31 16:18:50 +0700

    fix: fix direct import to lazy import

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,41 +1,41 @@
-import { createBrowserRouter,RouterProvider } from "react-router"
+import { createBrowserRouter, RouterProvider } from "react-router"
 import { lazy } from "react"
-import AdminCreateDeal from "@/pages/admin/AdminCreateDeal";
-import AdminCreateSeller from "@/pages/admin/AdminCreateSeller";
-import AdminSellers from "@/pages/admin/AdminSellers";
 
 const MainLayout = lazy(() => import("../layouts/MainLayout"));
-const Home = lazy(()=> import("../pages/Home"));
-const NotFound = lazy(()=> import("../pages/NotFound"))
-const About = lazy(()=> import("../pages/About"))
-const Coupons = lazy(()=> import("../pages/Coupons"))
-const Contacts = lazy(()=> import("../pages/Contacts"))
-const OTPPage = lazy(()=> import("../pages/OTPPage"))
-const DealPage = lazy(()=> import("../pages/DealPage"))
-const AdminUsers = lazy(()=> import("../pages/admin/AdminUsers"))
-const AdminDeals = lazy(()=> import("../pages/admin/AdminDeals"))
-const AdminAmount = lazy(()=> import("../pages/admin/AdminAmount"))
-const AdminTopDeals = lazy(()=> import("../pages/admin/AdminTopDeals"))
-const SearchDeal = lazy(()=> import("../pages/SearchDeal"))
+const Home = lazy(() => import("../pages/Home"));
+const NotFound = lazy(() => import("../pages/NotFound"))
+const About = lazy(() => import("../pages/About"))
+const Coupons = lazy(() => import("../pages/Coupons"))
+const Contacts = lazy(() => import("../pages/Contacts"))
+const OTPPage = lazy(() => import("../pages/OTPPage"))
+const DealPage = lazy(() => import("../pages/DealPage"))
+const AdminUsers = lazy(() => import("../pages/admin/AdminUsers"))
+const AdminDeals = lazy(() => import("../pages/admin/AdminDeals"))
+const AdminCreateDeal = lazy(() => import("../pages/admin/AdminCreateDeal"))
+const AdminCreateSeller = lazy(() => import("../pages/admin/AdminCreateSeller"))
+const AdminSellers = lazy(() => import("../pages/admin/AdminSellers"))
+const AdminAmount = lazy(() => import("../pages/admin/AdminAmount"))
+const AdminTopDeals = lazy(() => import("../pages/admin/AdminTopDeals"))
+const SearchDeal = lazy(() => import("../pages/SearchDeal"))
 
 function AppRouter() {
     const guestRouter = createBrowserRouter([
         {
             path: "/", element: <MainLayout />, children: [
                 { index: true, element: <Home /> },
-                { path: "about", element: <About />},
-                { path: "coupon", element: <Coupons/>},
-                { path: "contact", element: <Contacts />},
-                { path: "deal/:id", element: <DealPage />},
-                { path: "confirmEmail", element: <OTPPage />},
-                { path: "admin-users", element: <AdminUsers />},   
-                { path: "admin-sellers", element: <AdminSellers />},   
-                { path: "admin-deal", element: <AdminDeals />},                
-                { path: "admin-amount", element: <AdminAmount />},
-                { path: "admin-top-deals", element: <AdminTopDeals />},
-                { path: "admin-create-deal", element: <AdminCreateDeal />},
-                { path: "admin-create-seller", element: <AdminCreateSeller />},
-                { path: "searchDeal", element: <SearchDeal />},
+                { path: "about", element: <About /> },
+                { path: "coupon", element: <Coupons /> },
+                { path: "contact", element: <Contacts /> },
+                { path: "deal/:id", element: <DealPage /> },
+                { path: "confirmEmail", element: <OTPPage /> },
+                { path: "admin-users", element: <AdminUsers /> },
+                { path: "admin-sellers", element: <AdminSellers /> },
+                { path: "admin-deal", element: <AdminDeals /> },
+                { path: "admin-amount", element: <AdminAmount /> },
+                { path: "admin-top-deals", element: <AdminTopDeals /> },
+                { path: "admin-create-deal", element: <AdminCreateDeal /> },
+                { path: "admin-create-seller", element: <AdminCreateSeller /> },
+                { path: "searchDeal", element: <SearchDeal /> },
             ]
         },
         {
@@ -54,7 +54,7 @@ function AppRouter() {
         //         <Route path="*" element={<NotFound />} />
         //     </Routes>
         // </BrowserRouter>
-        <RouterProvider router={guestRouter}/>
+        <RouterProvider router={guestRouter} />
     )
 }
 
