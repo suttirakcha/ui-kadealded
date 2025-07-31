9c29169e1af451bf9f4ed988b61b3c08a2d75456
Lordalpaca <althanida@gmail.com>
2025-07-31 17:47:01 +0700

    style: adjust sidebar style

--- src/pages/admin/AdminStats.jsx
+++ src/pages/admin/AdminStats.jsx
@@ -1,11 +1,10 @@
 import { BarGraphComp } from "@/components/charts/BarGraphComp";
 import { DonutActiveComp } from "@/components/charts/DonutActiveComp";
 import { LineChartComp } from "@/components/charts/LineChartComp";
-import Sidebar from "@/components/custom/Sidebar";
 
 function AdminStats() {
   return (
-      <div className="max-w-[1200px] mx-auto w-full">
+      <div className="max-w-[1200px] mx-auto w-full ml-[158px]">
       <h1 className="w-full font-bold text-3xl py-5">Statistics</h1>
       <div className="grid grid-cols-2 gap-13">
         <LineChartComp />
