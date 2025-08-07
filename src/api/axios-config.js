26a3aaeaa510f954e8286a4d74e3d63b1a3954d5
suttirakcha <suttirak.char@gmail.com>
2025-08-07 22:51:39 +0700

    fix: refresh token error

---
+++ src/api/axios-config.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: "http://localhost:8000",
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalRequest = err.config;
+    if (
+      err.response.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/refresh")
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const response = await instance.get("/api/refresh", {
+          _retry: true,
+        });
+        const newAccessToken = response.data.accessToken;
+
+        localStorage.setItem("accessToken", newAccessToken);
+        instance.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${newAccessToken}`;
+        originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+
+        return instance(originalRequest);
+      } catch (error) {
+        console.error("Refresh token failed:", error);
+        localStorage.removeItem("accessToken");
+        return Promise.reject(error);
+      }
+    }
+  }
+);
