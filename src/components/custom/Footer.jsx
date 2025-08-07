b12a29384df4b57e4ee550a407efb2962186dc5c
suttirakcha <suttirak.char@gmail.com>
2025-08-07 13:14:46 +0700

    feat: update footer menus

--- src/components/custom/Footer.jsx
+++ src/components/custom/Footer.jsx
@@ -8,11 +8,11 @@ function Footer() {
 
   useEffect(() => {
     const run = async () => {
-      await fetchAllCategories()
-    }
+      await fetchAllCategories();
+    };
 
-    run()
-  }, [])
+    run();
+  }, []);
 
   // console.log(categories);
   // const categories = [
@@ -25,52 +25,53 @@ function Footer() {
   //   "Category 4",
   // ];
 
-  const footerLinks = [
+  const footerLinksLeft = [
+    { text: "Search deal", href: "/searchDeal" },
+  ];
+
+  const footerLinksRight = [
     { text: "About us", href: "/about" },
     { text: "Contact us", href: "/contact" },
     { text: "Partners", href: "" },
     { text: "Careers", href: "" },
     { text: "Terms and Conditions", href: "" },
-    { text: "Privacy Policy", href: "" }
-  ]
+    { text: "Privacy Policy", href: "" },
+  ];
 
   return (
     <footer className="bg-[#004066] w-full">
-      <div className="grid grid-cols-5 max-w-[1200px] w-full p-8 items-center mx-auto">
+      <div className="grid grid-cols-4 max-w-[1200px] w-full p-8 items-start mx-auto gap-8">
+        {/* section1 */}
+        <div className="flex flex-col">
+          <img
+            src="/src/assets/kaDEALded_logo-removebg-preview.png"
+            alt="kadealded logo with no backgorund"
+          />
+        </div>
         {/* section2 */}
-        <div className="text-white flex flex-col items-start">
-          {categories?.map((category, index) => (
-            <Link key={index} to="#">
-              {category.name}
+        <div className="text-white flex flex-col pt-10">
+          {footerLinksLeft.map((link, index) => (
+            <Link key={index} to={link.href} className="w-fit">
+              {link.text}
             </Link>
           ))}
         </div>
 
-        {/* section1 */}
-        <div className="flex flex-col items-center col-span-3">
-          <div className="w-1/3">
-            {/* web logo */}
-            <img
-              src="/src/assets/kaDEALded_logo-removebg-preview.png"
-              alt="kadealded logo with no backgorund"
-            />
-          </div>
-          {/* texts and social logos */}
+        {/* section3 */}
+        <div className="text-white flex flex-col pt-10">
+          {footerLinksRight.map((link, index) => (
+            <Link key={index} to={link.href} className="w-fit">
+              {link.text}
+            </Link>
+          ))}
+        </div>
+        <div className="pt-10">
           <p className="text-white mb-2">SUPPORT US AT</p>
           <span className="text-white flex gap-4">
             <Instagram />
             <Youtube />
             <Linkedin />
           </span>
-        </div>
-
-        {/* section3 */}
-        <div className="text-white flex flex-col items-end">
-          {footerLinks.map((link, index) => (
-            <Link key={index} to={link.href}>
-              {link.text}
-            </Link>
-          ))}
         </div>
       </div>
     </footer>
