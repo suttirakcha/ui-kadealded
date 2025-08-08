adb96d599dabdb3791906fd196506f6a26d619f4
jobtnbd17 <jobtnbd@gmail.com>
2025-08-08 11:33:41 +0700

    feat:payment

---
+++ src/stores/usePayment.js
@@ -0,0 +1,11 @@
+import { authApi } from "@/api/routesApi";
+
+
+
+
+export const usePayment = async(data) => {
+  const response = await authApi.post('/payment',data)
+  console.log('response', response.data)
+  window.location.href = response.data.url
+
+}
